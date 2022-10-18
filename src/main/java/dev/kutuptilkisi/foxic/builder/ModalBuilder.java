2dc275fa1727e0510c2934a421ba8932d9db8af3
BuildTools <unconfigured@null.spigotmc.org>
2022-10-18 16:40:19 +0300

    Update 1.0.3 - Added Modal Support

---
+++ src/main/java/dev/kutuptilkisi/foxic/builder/ModalBuilder.java
@@ -0,0 +1,53 @@
+package dev.kutuptilkisi.foxic.builder;
+
+import dev.kutuptilkisi.foxic.handler.ViewHandler;
+import net.dv8tion.jda.api.interactions.components.Modal;
+import net.dv8tion.jda.api.interactions.components.text.TextInput;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModalBuilder {
+    private final ViewHandler handler;
+
+    private String id;
+
+    private String title;
+
+    private List<TextInput> fields;
+
+    private ViewHandler.ModalCallback callback;
+
+    public ModalBuilder(ViewHandler handler){
+        this.handler = handler;
+        fields = new ArrayList<>();
+    }
+
+    public ModalBuilder setID(String id){
+        this.id = id;
+        return this;
+    }
+
+    public ModalBuilder setTitle(String title){
+        this.title = title;
+        return this;
+    }
+
+    public ModalBuilder addTextField(TextInput field){
+        fields.add(field);
+        return this;
+    }
+
+    public ModalBuilder setCallback(ViewHandler.ModalCallback callback){
+        this.callback = callback;
+        return this;
+    }
+
+    public Modal build(){
+        if(id == null || title == null){
+            throw new RuntimeException("ID and title must be set");
+        }
+        handler.listen(id, callback);
+        return Modal.create(id, title).addActionRow(fields).build();
+    }
+}
--- src/main/java/dev/kutuptilkisi/foxic/handler/ViewHandler.java
+++ src/main/java/dev/kutuptilkisi/foxic/handler/ViewHandler.java
@@ -1,9 +1,11 @@
 package dev.kutuptilkisi.foxic.handler;
 
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.UserContextInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.Modal;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import org.jetbrains.annotations.NotNull;
 
@@ -14,42 +16,43 @@ public class ViewHandler extends ListenerAdapter implements ComponentHandler{
     public interface ButtonCallback {
         void call(ButtonInteractionEvent event);
     }
+
+    public interface ModalCallback {
+        void call(ModalInteractionEvent event);
+    }
+
     private final JDA jda;
 
-    private final HashMap<String, ButtonCallback> callbackHashMap;
+    private final HashMap<String, ButtonCallback> buttonCallbackHashMap;
+    private final HashMap<String, ModalCallback> modalCallbackHashMap;
 
     public ViewHandler(JDA jda){
         this.jda = jda;
-        callbackHashMap = new HashMap<>();
+        buttonCallbackHashMap = new HashMap<>();
+        modalCallbackHashMap = new HashMap<>();
 
         this.jda.addEventListener(this);
     }
 
-    public void listen(Button button, ButtonCallback callback){
-        this.listen(button.getId(), callback);
-    }
-
-    public void stopListen(Button button){
-        this.stopListen(button.getId());
-    }
-
     public void listen(String id, ButtonCallback callback){
-        callbackHashMap.put(id, callback);
+        buttonCallbackHashMap.put(id, callback);
     }
 
-    public void stopListen(String id){
-        callbackHashMap.remove(id);
+    public void listen(String id, ModalCallback callback){
+        this.modalCallbackHashMap.put(id, callback);
     }
 
     @Override
     public void onButtonInteraction(@NotNull ButtonInteractionEvent event) {
-        if(callbackHashMap.containsKey(event.getComponentId())){
-            callbackHashMap.get(event.getComponentId()).call(event);
+        if(buttonCallbackHashMap.containsKey(event.getComponentId())){
+            buttonCallbackHashMap.get(event.getComponentId()).call(event);
         }
     }
 
     @Override
-    public void onUserContextInteraction(@NotNull UserContextInteractionEvent event) {
-        super.onUserContextInteraction(event);
+    public void onModalInteraction(@NotNull ModalInteractionEvent event) {
+        if(modalCallbackHashMap.containsKey(event.getModalId())){
+            modalCallbackHashMap.get(event.getModalId()).call(event);
+        }
     }
 }
